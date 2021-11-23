602d85e10928facaa1b01a4f8aede2ae064d115e
manu-devops98 <manuelcappello7@gmail.com>
2021-11-23 15:33:36 +0100

    add esercizio

--- js/script.js
+++ js/script.js
@@ -6,5 +6,21 @@ for (let i = 1; i <= 100; i++) {
     element.classList.add('box');
     // console.log(element);
     element.innerHTML = i;
+    
+
+    if (i % 15 == 0) {
+        element.classList.add('red');
+        element.innerHTML = 'fizzbuzz';
+        
+    } else if (i % 5 == 0) {
+        element.classList.add('yellow');
+        element.innerHTML = 'buzz';
+    } else if (i % 3 == 0) {
+        element.classList.add('green');
+        element.innerHTML = 'fizz';
+    } else {
+        element.classList.add('blue');
+    }
+
     container.append(element);
 }
